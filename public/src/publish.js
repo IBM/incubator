0c3da9f99946e38cb3fab7735fec6c0b41f07fd4
antonm <antonm@ca.ibm.com>
2017-12-11 09:20:13 -0500

    Updated to work from a JSON data model ...
    Added in a tile for workshops.
    Added in a js file and json file to model the publications as data, and
    data drive the creation of the web page.

---
+++ public/src/publish.js
@@ -0,0 +1,30 @@
+function createTile(publication) {
+
+  var anchor = document.getElementById('anchor');
+
+  var link = document.createElement('a');
+
+  link.className = 'tile';
+  link.href = publication.repo + '/' + publication.file;
+
+  link.innerHTML = '<div class="control">' +
+  '<div class="visual"><img class="image" src="./images/' + publication.icon + '"></div>' +
+  '<div class="type">' + publication.type + '</div>' +
+
+  // '<div class="description">' + publication.description + '</div>' +
+  '</div>'
+
+  anchor.appendChild(link);
+}
+
+function reqListener() {
+  var publicions = JSON.parse(this.responseText)
+  publicions.forEach(function(publication) {
+    createTile(publication);
+  })
+}
+
+var request = new XMLHttpRequest();
+request.addEventListener("load", reqListener);
+request.open("GET", "./data/publications.json");
+request.send();
